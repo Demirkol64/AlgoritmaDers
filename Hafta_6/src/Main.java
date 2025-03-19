461ae08b5bbd0ce340ec58310af25aeeab8e7710
Recep Ali Demirkol <demirkol6486@gmail.com>
2025-03-19 09:54:55 +0300

    Update Main.java

--- Hafta_6/src/Main.java
+++ Hafta_6/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int[] nums1 = {4,3,5,1,6,7,8,2};
-        //Sotring.insertion(nums1);
+        //Sorting.insertion(nums1);
         //Sorting.bubble(nums1);
     }
 }
